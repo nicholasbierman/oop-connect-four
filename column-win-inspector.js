0f5e04c0074f5a723c2baca1da9628969ad7f766
Nick <njb8ah@virginia.edu>
2020-10-08 19:16:21 -0400

    began working on determining a column win (not yet completed)

---
+++ column-win-inspector.js
@@ -0,0 +1,13 @@
+export default class ColumnWinInspector {
+     constructor(column) {
+          this.column = column;
+     }
+
+     inspect() {
+          let player1MaxNumber = 0;
+          let player2MaxNumber = 0;
+          for (let i = 0; i < this.column.tokens.length; i++) {
+               
+          }
+     }
+}
