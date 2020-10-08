49388dce10458010825b0440ba7959099c2ba2f2
tony <tony@suas.com>
2020-10-08 16:40:46 -0400

    Implement colum halfway

---
+++ colum.js
@@ -0,0 +1,19 @@
+export default class Column{
+  constructor(){
+    this.tokens = [null, null, null, null, null, null];
+  }
+
+  add(currentPlayer) {
+    for(let i = 5; i >= 0; i--){
+      if(this.tokens[i] === null){
+        this.tokens[i] = currentPlayer;
+        return;
+      }
+    }
+  }
+  // returns null, 1 or 2
+  getTokenAt(rowIndex){
+    //if(rowIndex >= this.tokens.length) return null;
+    return this.tokens[rowIndex];
+  }
+}
